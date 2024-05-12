a048790dc12ce744d9b0dadbf7cb9cfde7260a7a
ASADULLAH SHAFIQUE <asadullahshafique@hotmail.com>
2024-05-12 10:30:49 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ANSWER37
+ Large Shirts: Default values in make_shirt().
---
+++ ans37.js
@@ -0,0 +1,6 @@
+//Large Shirts: Default values in make_shirt().
+function manufactingT_shirt(Sizes, Print) {
+    console.log("Manufacturing a Tshirt of ".concat(Sizes, " with the message \"").concat(Print, " print on it."));
+}
+manufactingT_shirt("large 34", "Can't HEAR you I'm C O D I N G");
+manufactingT_shirt("small 30", "C #");
